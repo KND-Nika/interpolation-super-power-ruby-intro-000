88eb868b296f3dc3d8714588a51a5b0bcf72e63c
Nika Jay <venika.jarvis@gmail.com>
2021-01-13 02:41:05 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,4 +7,4 @@ def display_rainbow (color)
   puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
 end
 
-print display_rainbow (color)
+
